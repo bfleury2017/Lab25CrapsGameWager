e9513d4919bd6ed2065ad79c038188c2f6fdb041
bfleury2017 <fleury.brian@gmail.com>
2017-11-25 19:14:34 -0500

    Initial

---
+++ Craps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab25CrapsGameWager
+{
+    class Craps
+    {
+        
+        public static void Main(string[] args)
+        {
+            var game = new GameApp();
+
+            Console.Write("Play a game? [Y/N]: ");
+            char response = Console.ReadLine().ToUpper()[0];
+            var playGame = response.Equals('Y') ? true : false;
+
+            while (playGame)
+            {
+                Console.Write($"Enter a wager or -1 to quit (Player balance {game.Balance:C}): ");
+                game.Wager = ValidateWager(game.Balance);
+
+                if (game.Wager == -1) break;
+
+                game.Play();
+                Console.WriteLine();
+
+                if (game.Balance <= 0) break;
+
+            }
+
+            Console.WriteLine("Good bye.");
+        }
+
+        private static decimal ValidateWager(decimal balance)
+        {
+            var wager = decimal.Parse(Console.ReadLine());
+
+            while (wager <= 0 || wager > balance)
+            {
+                Console.Write($"{wager} is invalid, must be greater than 0 and cannot exceed {balance:C}: ");
+                wager = decimal.Parse(Console.ReadLine());
+            }
+            return wager;
+        }
+    }
+}
---
+++ GameApp.cs
@@ -0,0 +1,102 @@
+﻿using System;
+namespace Lab25CrapsGameWager
+{
+    enum Status { Continue, Won, Lost }
+
+    class GameApp
+    {
+        private static Random randomNumbers = new Random();
+
+        private enum DiceNames
+        {
+            SnakeEyes = 2,
+            Trey = 3,
+            Seven = 7,
+            YoLeven = 11,
+            BoxCars = 12
+        }
+
+
+        public Status GameStatus { get; set; }
+        private decimal balance;
+
+        public decimal Balance {
+            get { return balance; }
+            set { if (value >= 0) balance = value; }
+        }
+
+        private decimal wager;
+        public decimal Wager {
+            get { return wager; }
+            set { wager = value; }
+        }
+
+        public GameApp()
+        {
+            Balance = 1000;
+            GameStatus = Status.Continue;
+        }
+
+        public void Play() {
+            int point = 0;
+            int sumDice = RollDice();
+
+            switch ((DiceNames) sumDice)
+            {
+                case DiceNames.Seven:
+                case DiceNames.YoLeven:
+                    GameStatus = Status.Won;
+                    break;
+                case DiceNames.SnakeEyes:
+                case DiceNames.Trey:
+                case DiceNames.BoxCars:
+                    GameStatus = Status.Lost;
+                    break;
+                default:
+                    GameStatus = Status.Continue;
+                    point = sumDice;
+                    Console.WriteLine($"Point {point}");
+                    break;
+            }
+
+
+            while (GameStatus == Status.Continue)
+            {
+                sumDice = RollDice();
+                if (point == sumDice)
+                {
+                    GameStatus = Status.Won;
+                } else {
+                    if (sumDice == (int)DiceNames.Seven)
+                    {
+                        GameStatus = Status.Lost;
+                    }
+                }
+            }
+
+            var result = "";
+            if (GameStatus == Status.Won)
+            {
+                Balance += Wager;
+                result = "Player wins";
+
+            } else
+            {
+                Balance -= Wager;
+                result = "Player loses";
+            }
+
+            var bustedPrompt = Balance > 0 ? "" : "Busted.";
+            Console.WriteLine($"{result}. {bustedPrompt}");
+        }
+
+        public int RollDice(){
+            int die1 = randomNumbers.Next(1, 7);
+            int die2 = randomNumbers.Next(1, 7);
+
+            int sum = die1 + die2;
+            Console.WriteLine($"Player rolled {die1} + {die2} = {sum}");
+            return sum;
+        }
+    }
+}
